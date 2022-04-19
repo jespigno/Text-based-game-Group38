03f588b329978db179c301a682b0496c1942d7d4
Jose Espino <98888659+jespigno@users.noreply.github.com>
2022-04-19 10:40:00 +0800

    Added struct

--- main.cpp
+++ main.cpp
@@ -5,6 +5,29 @@ using namespace std;
 #include "TimeIntervals.h"
 
 int main(){
+  struct CharacterInformation{
+    int level = 1;
+    int maxhealth = 10;
+    int mycurrenthealth = 10;
+    int mydefense = 3;
+    int myattack = 3;
+    int myspeed = 3;
+    int mymoney = 0;
+    float experiencepoints = 0.0;
+    string myname = "Joe";
+    int catnumber = 0;
+    int amountoffurniture = 0;
+    bool boughtcatcafe = false;
+    bool level1complete = false;
+    bool level2complete = false;
+    bool level3complete = false;
+    bool level4complete = false;
+    bool level5complete = false;
+  };
+
+  CharacterInformation level = 1, maxhealth = 10, mycurrenthealth = 10, mydefense = 3, myattack = 3, myspeed = 3, mymoney = 0, experiencepoints = 0.0;
+  CharacterInformation myname = "Joe", catnumber = 0, boughtcafe = false, level1complete = false, level2complete = false, level3complete = false, level4complete = false, level5complete = false;
+
   char saveornot;
   cout<<R"(
     @@@@@@@@@@@@@@@@&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -99,7 +122,7 @@ int main(){
     cout<< "Load game (L) or New game (N)?"<<endl;
     cin >> saveornot;
     if (saveornot == 'L'){
-      loadgame(); //need to code loadgame() function
+      loadgame(CharacterInformation); //need to code loadgame() function
       break;
     }else if (saveornot == 'N'){
       break;
